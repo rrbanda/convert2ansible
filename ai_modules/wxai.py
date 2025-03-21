3fa16146b0f8333096bbc8d720796a09795732d8
Brian S Paskin <brian@paskino.com>
2025-03-21 18:24:16 +0000

    use watsonx.ai api

--- ai_modules/wxai.py
+++ ai_modules/wxai.py
@@ -1,5 +1,7 @@
 from .explanator import Explanator
-import requests
+from ibm_watsonx_ai import APIClient
+from ibm_watsonx_ai import Credentials
+from ibm_watsonx_ai.foundation_models import ModelInference
 from fileio.settings import Settings
 
 class WxAI(Explanator):
@@ -14,6 +16,11 @@ class WxAI(Explanator):
         self.api_key = wxai_apikey
         self.project_id = wxai_project
 
+        self.credentials = Credentials(
+           url = "https://us-south.ml.cloud.ibm.com", 
+           api_key = wxai_apikey
+        )
+
     def who_am_i(self):
         super().who_am_i()
 
@@ -22,91 +29,43 @@ class WxAI(Explanator):
         print(f"API KEY: {self.api_key}")
 
     def retrieve_tokens(self):
-        # Retrieve token for REST service
-        d = {
-            "grant_type": "urn:ibm:params:oauth:grant-type:apikey",
-            "apikey": self.api_key,
-        }
-        token_response = requests.post(
-            "https://iam.cloud.ibm.com/identity/token",
-            headers={"Content-Type": "application/x-www-form-urlencoded"},
-            data=d,
-        )
-
-        if token_response.status_code != 200:
-            print(
-                "error: bad token_response from access token service:"
-                + token_response.text
-            )
-            return {
-                "headers": {"Content-Type": "application/json; charset=utf-8"},
-                "statusCode": token_response.status_code,
-                "body": token_response.text,
-            }
-        token = token_response.json()["access_token"]
-        return token
-    
-    # Assisted by WCA@IBM
-    # Latest GenAI contribution: ibm/granite-8b-code-instruct
-    """
-    Generates an explanation for the provided code using IBM watsonx AI.
-
-    Returns:
-        str: The generated explanation.
-    """
+        super.tokens()
    
     def transform(self, code, context):
-        token = self.retrieve_tokens()
-
-        url = (
-            "https://us-south.ml.cloud.ibm.com/ml/v1/text/generation?version=2023-05-29"
+        client = APIClient(self.credentials)
+        reasoning_model = ModelInference(
+            model_id=self.model_name,
+            api_client=client,
+            project_id=self.project_id
         )
-        summary_request = f"""As a software developer take the {context} and change it to a Ansible Playbook and only provide the yaml code and do not display any comments or explanation
 
-        [INPUT]
+
+        prompt = f"""As a software developer take the {context} and change it to a Ansible Playbook and only provide the yaml code and do not display thought process or any comments or explanation: 
         {code}
-
-        [SUMMARY]
         """
-        messages = [{
+
+        messages = [
+            {
                 "role": "control",
                 "content": "thinking"
             },
             {
                 "role": "user",
-                "content": [{
+                "content": [
+                    {
                         "type": "text",
-                        "text": f"{summary_request}"
-                }]
+                        "text": f"{prompt}"
+                    }
+                ]
             }
-         ]
+        ]
 
-        body = {
-            "parameters": {
-                "decoding_method": "greedy",
-                "max_new_tokens": 3000,
-                "repetition_penalty": 1,
-            },
-            "model_id": self.model_name,
-            "project_id": self.project_id,
-            "input": summary_request,
-        }
 
-        headers = {
-            "Accept": "application/json",
-            "Content-Type": "application/json",
-            "Authorization": f"Bearer {token}",
-        }
-
-        response = requests.post(url, headers=headers, json=body)
-
-        if response.status_code != 200:
-            raise Exception("Non-200 response: " + str(response.text))
-
-       # print(response.json())
-        data = response.json()
+        output = reasoning_model.chat(messages=messages)['choices'][0]['message']['content']
+        return output
+    
        # print(data['results'][0]['generated_text'])
-        return data['results'][0]['generated_text']
+       # return data['results'][0]['generated_text']
     
     def answer_user_query(self,question,references):	
         return
